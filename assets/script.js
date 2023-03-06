6297d759e03250fc48ac7c4b9b7bffaf31c83d24
Tim Aspesberro <taspes@gmail.com>
2023-03-06 00:31:57 -0500

    added js code

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,14 @@ var answerOne = document.getElementById("answerOne");
 var answerTwo = document.getElementById("answerTwo");
 var answerThree = document.getElementById("answerThree");
 var answerFour = document.getElementById("answerFour");
-var startBtn = document.getElementById("start-btn");
-var highscoreBtn = document.getElementById("#highscore-btn")
+var answerButtons = document.getElementById("answer-buttons");
+var startBtn = document.getElementById("start-button");
+var highscoreBtn = document.getElementById("highscore-btn");
+
+var answerEl = `<button class="btn" id="answerOne">Answer 1</button>
+<button class="btn" id="answerTwo">Answer 2</button>
+<button class="btn" id="answerThree">Answer 3</button>
+<button class="btn" id="answerFour">Answer 4</button>`;
 
 const questions = ['Which of these is not a javascript data type?', 'Which html tag is used to create a navigation bar?', 'Which of these is the css universal selector?', 'Which one of these is not a programming language?'];
 
@@ -16,11 +22,14 @@ const questionFourAns = ['Javascript', 'C#', 'Python', 'Bootstrap']
 const correct = 0;
 
 
-var fillEl = () => {
-
+const startQuiz = () => {
+    setTimeout (() => {
+        startBtn.remove();
+        answerButtons.innerHTML += `${answerEl}`
+    }, 600);
 }
-
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
-})
+    startQuiz();
+});
